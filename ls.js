3bdacebf0988a3ef1162b658aef5532952fb56b8
nqdinh <nqdinhddt@gmail.com>
2016-10-13 21:07:14 +0700

    cleanup with each in promise

--- ls.js
+++ ls.js
@@ -17,18 +17,15 @@ async function ls(loc) {
     filePath = path.join(__dirname, filePath)
   }
   fs.promise.readdir(filePath)
-    .then((items) => {
-      items.forEach((i) => {
-        fs.promise.lstat(path.join(loc, i))
-          .then((stat) => {
-            if (stat.isDirectory()) {
-              // console.log(path.join(loc, i, '/'))
-              ls(path.join(loc, i))
-            } else {
-              console.log(path.join(loc, i))
-            }
-          })
-      })
+    .each((item) => {
+      fs.promise.lstat(path.join(loc, item))
+        .then((stat) => {
+          if (stat.isDirectory()) {
+            ls(path.join(loc, item))
+          } else {
+            console.log(path.join(loc, item))
+          }
+        })
     })
     .catch((e) => {
       // the file not existing or access denied
