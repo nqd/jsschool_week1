f40c10f1b9e4f56cdeecf98059d46124d205fe64
nqdinh <nqdinhddt@gmail.com>
2016-10-17 09:45:47 +0700

    begin rm

--- rm.js
+++ rm.js
@@ -10,24 +10,20 @@ require('./helper')
 const fs = require('fs').promise
 const path = require('path')
 
-async function mkdir(loc) {
+async function rm(loc) {
   if (!loc) return
   let dirPath = loc
   if (!path.isAbsolute(dirPath)) {
     dirPath = path.join(__dirname, dirPath)
   }
+  console.log(dirPath);
 
-  fs.mkdir(dirPath)
+  fs.rm(dirPath)
     .catch((e) => {
       console.log('Error:', e.code)
     })
 }
 
-if (process.argv.length !== 3) {
-  console.log('Error: Run with %s dir', process.argv[1])
-  process.exit(0)
-}
+const loc = process.argv[2]
 
-const dir = process.argv[2]
-
-mkdir(dir)
+rm(loc)
